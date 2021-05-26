d57ffe883c1ea5ce0e19697bb1a932c1ba424be4
Martin <42607090+h574641@users.noreply.github.com>
2021-05-26 21:46:44 +0200

    Har lagt til menyer i egne scener.

---
+++ BachelorScene/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+   public void StartTutorial()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void StartSimulation()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void ExitGame()
+    {
+        Application.Quit();
+    }
+
+    public void StartMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+}
